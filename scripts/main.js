ddbc5fab067090dc9db0b7184578f8dbf01b0f47
Artem <asustransformertf300x@gmail.com>
2021-12-09 11:17:24 +0300

    Created a Video block

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+(function(){
+    const header = document.querySelector(".header");
+    window.onscroll = () => {
+        if(window.scrollY > 50) header.classList.add("header_active")
+        else header.classList.remove("header_active")
+    }
+}());
